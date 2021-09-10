6060fb79a9b899cf91251b5b90a1b7688aa5af7b
mbote <mbotepm@gmail.com>
2021-09-10 17:01:42 +0100

    adding category model

--- src/app/in-memmory-database.ts
+++ src/app/in-memmory-database.ts
@@ -1,15 +1,15 @@
-import {InMemoryDbService} from 'angular-in-memory-web-api';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Category } from './pages/categories/shared/category.model'
+export class InMemoryDataBase implements InMemoryDbService {
 
-export class InMemoryDataBase implements InMemoryDbService{
-
-    createDb(){
-        const categories = [
-            {id:1, name:"Moradia", description:"Pagamento de conta da casa"},
-            {id:2, name:"Saúde", description:"Plano de saúde e remédios"},
-            {id:3, name:"Lazer", description:"Cinema, parque, praia, etc."},
-            {id:4, name:"Salário", description:"Recebimento de salário"},
-            {id:5, name:"Freelas", description:"Trabalho com freelancer"}
+    createDb() {
+        const categories: Category[] = [
+            { id: 1, name: "Moradia", description: "Pagamento de conta da casa" },
+            { id: 2, name: "Saúde", description: "Plano de saúde e remédios" },
+            { id: 3, name: "Lazer", description: "Cinema, parque, praia, etc." },
+            { id: 4, name: "Salário", description: "Recebimento de salário" },
+            { id: 5, name: "Freelas", description: "Trabalho com freelancer" }
         ];
-        return {categories};
+        return { categories };
     }
 }
