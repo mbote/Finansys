b531bcc25168aa5d58acbc8d90a2cbfa29373e96
mbote <mbotepm@gmail.com>
2021-09-10 18:50:39 +0100

    intercepting http requests with angular-in-memory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDataBase} from './in-memmory-database'
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataBase)
   ],
   providers: [],
   bootstrap: [AppComponent]
