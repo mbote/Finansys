d80f2e93b9c28bc660475c1ee5cfc68ee4524ceb
mbote <mbotepm@gmail.com>
2021-09-22 11:37:21 +0100

    improving entryService

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -6,34 +6,30 @@ import { map, catchError, flatMap } from 'rxjs/Operators';
 
 import { Entry } from './entry.model';
 import { CategoryService } from '../../categories/shared/category.service';
-
 @Injectable({
   providedIn: 'root'
 })
 export class EntryService extends BaseResourceService<Entry> {
 
-
   constructor(protected injector: Injector, private categoryService: CategoryService) {
-      super("api/entries", injector, Entry.fromJson);
-   }
-
+    super("api/entries", injector, Entry.fromJson);
+  }
 
   create(entry: Entry): Observable<Entry> {
-    return this.categoryService.getId(entry.categoryId!).pipe(
-      flatMap(category => {
-        entry.category = category;
-        return super.create(entry);
-      })
-    )
+    return this.setCategoryAndSendToServer(entry, super.create.bind(this));
   }
 
   update(entry: Entry): Observable<Entry> {
+    return this.setCategoryAndSendToServer(entry, super.update.bind(this));
+  }
+
+  private setCategoryAndSendToServer(entry: Entry, sendFn: any): Observable<any> {
     return this.categoryService.getId(entry.categoryId!).pipe(
       flatMap(category => {
         entry.category = category;
-        return super.update(entry);
-      })
+        return sendFn(entry);
+      }),
+      catchError(this.handleError)
     )
   }
-
 }
