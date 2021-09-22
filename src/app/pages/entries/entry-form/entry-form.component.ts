3e958fa24afa6c05cb1423ae2c32aa88d2638a27
mbote <mbotepm@gmail.com>
2021-09-22 11:09:23 +0100

    improving BaseResourceService && entryService && categoryService

--- src/app/pages/entries/entry-form/entry-form.component.ts
+++ src/app/pages/entries/entry-form/entry-form.component.ts
@@ -100,7 +100,7 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
   }
 
   createEntry() {
-    const entry: Entry = Object.assign(new Entry(), this.entryForm.value);
+    const entry: Entry = Entry.fromJson(this.entryForm.value);
     this.entryService.create(entry)
       .subscribe(
         entry => this.actionsForSuccess(entry),
@@ -109,7 +109,7 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
   }
 
   updateEntry() {
-    const entry: Entry = Object.assign(new Entry(), this.entryForm.value);
+    const entry: Entry = Entry.fromJson(this.entryForm.value);
     this.entryService.update(entry)
       .subscribe(
         entry => this.actionsForSuccess(entry),
--- src/app/shared/services/base-resource.services.ts
+++ src/app/shared/services/base-resource.services.ts
@@ -2,61 +2,65 @@ import { BaseResourceModel } from '../models/base-resource.model';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { map, catchError, flatMap } from 'rxjs/Operators';
-import {Injector} from '@angular/core';
+import { Injector } from '@angular/core';
 
 export abstract class BaseResourceService<T extends BaseResourceModel>{
 
     protected http: HttpClient;
 
-    constructor(protected api: string,  protected injector: Injector){
+    constructor(
+        protected api: string,
+        protected injector: Injector,
+        protected jsonDataToResourceFn: (jsonData: any) => T
+    ) {
         this.http = injector.get(HttpClient);
     }
 
     getAll(): Observable<T[]> {
-        return this.http.get(this.api).pipe(
-            catchError(this.handleError),
-            map(this.jsonDataToResources)
+        return this.http.get<any>(this.api).pipe(
+            map(this.jsonDataToResources.bind(this)),
+            catchError(this.handleError)
         );
     }
 
     getId(id: number): Observable<T> {
         const url = `${this.api}/${id}`;
         return this.http.get(url).pipe(
-            catchError(this.handleError),
-            map(this.jsonDataToResource)
+            map(this.jsonDataToResource.bind(this)),
+            catchError(this.handleError)
         );
     }
 
     create(resource: T): Observable<T> {
         return this.http.post(this.api, resource).pipe(
-            catchError(this.handleError),
-            map(this.jsonDataToResource)
+            map(this.jsonDataToResource.bind(this)),
+            catchError(this.handleError)
         );
     }
 
     update(resource: T): Observable<T> {
         const url = `${this.api}/${resource.id}`;
         return this.http.put(url, resource).pipe(
-            catchError(this.handleError),
-            map(() => resource)
+            map(() => resource),
+            catchError(this.handleError)
         );
     }
 
     delete(id: number): Observable<any> {
         const url = `${this.api}/${id}`;
         return this.http.delete(url).pipe(
-            catchError(this.handleError),
-            map(() => null)
+            map(() => null),
+            catchError(this.handleError)
         )
     }
 
     protected jsonDataToResource(jsonData: any): T {
-        return jsonData as T;
+        return this.jsonDataToResourceFn(jsonData);
     }
 
     protected jsonDataToResources(jsonData: any[]): T[] {
         const resources: T[] = [];
-        jsonData.forEach(element => resources.push(element as T));
+        jsonData.forEach(element => resources.push(this.jsonDataToResourceFn(element)));
         return resources;
     }
 
