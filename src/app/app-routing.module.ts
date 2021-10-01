9f975b30e32d666d64042845ebeacdf88d079a94
mbote <mbotepm@gmail.com>
2021-10-01 15:21:17 +0100

    adding reporting base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'categories', loadChildren: () => import("./pages/categories/categories.module").then(m => m.CategoriesModule)},
-  { path: 'entries', loadChildren:() => import("./pages/entries/entries.module").then(e => e.EntriesModule) }
+  { path: 'entries', loadChildren:() => import("./pages/entries/entries.module").then(e => e.EntriesModule) },
+  { path: 'reports', loadChildren:() => import("./pages/reports/reports.module").then(e => e.ReportsModule) }
 ];
 
 @NgModule({
